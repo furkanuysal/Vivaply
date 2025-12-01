b8dc8bdd64c0579964ba74d4497c17ecbe983b12
furkanuysal <60571747+furkanuysal@users.noreply.github.com>
2025-12-02 00:05:06 +0300

    Refactor .gitignore and implement ProfileController with JWT authentication and user profile retrieval

---
+++ Vivaply.API/DTOs/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Vivaply.API.DTOs
+{
+    public record UserProfileDto
+    (
+        string Username,
+        string Email,
+        int Level,
+        long CurrentXp,
+        long TotalXp,
+        decimal Money,
+        int CurrentStreak
+    );
+}
--- Vivaply.API/Program.cs
+++ Vivaply.API/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using Vivaply.API.Data;
 using Vivaply.API.Services;
 
@@ -15,23 +19,77 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 // Services Configuration
 builder.Services.AddControllers();
 
-// builder.Services.AddOpenApi();
-// Classic Swagger Configuration
+// CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        b => b
+            .WithOrigins("http://localhost:5173") // React URl
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()); // to allow cookies/auth headers
+});
+
+// JWT Authentication Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!)),
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            ValidateLifetime = true
+        };
+    });
+
+// Swagger Configuration
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Örnek: \"Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Http Request Pipeline Configuration
 if (app.Environment.IsDevelopment())
 {
-    // app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Activate CORS Middleware
+app.UseCors("AllowReactApp");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
