8855e5aecbec0b3ad33be05a602e2b326355b2ae
furkanuysal <60571747+furkanuysal@users.noreply.github.com>
2025-12-14 23:54:11 +0300

    feat(ratings): add rating colors to light and dark themes in CSS and Tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,6 +13,17 @@ export default {
           primary: "var(--color-primary-base)",
           secondary: "var(--color-secondary-base)",
           accent: "var(--color-accent)",
+
+          rating: {
+            95: "rgb(var(--rating-95) / <alpha-value>)",
+            90: "rgb(var(--rating-90) / <alpha-value>)",
+            80: "rgb(var(--rating-80) / <alpha-value>)",
+            70: "rgb(var(--rating-70) / <alpha-value>)",
+            60: "rgb(var(--rating-60) / <alpha-value>)",
+            45: "rgb(var(--rating-45) / <alpha-value>)",
+            bad: "rgb(var(--rating-bad) / <alpha-value>)",
+            none: "rgb(var(--rating-none) / <alpha-value>)",
+          },
         },
       },
       borderColor: ({ theme }) => ({
