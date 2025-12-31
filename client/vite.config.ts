a735b02be2e60b1a81287ad572e2e0871c25a973
furkanuysal <60571747+furkanuysal@users.noreply.github.com>
2025-12-31 15:32:59 +0300

    feat(ssl): add @vitejs/plugin-basic-ssl for improved HTTPS support in Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
