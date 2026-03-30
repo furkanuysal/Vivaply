e2c6d5d6489f4a3cec292eb581998d2e7ad4ed2d
furkanuysal <60571747+furkanuysal@users.noreply.github.com>
2026-03-30 23:50:32 +0300

    feat(controller): add BaseApiController with CurrentUserId property for user identification

---
+++ Vivaply.API/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Vivaply.API.Entities.Identity;
+
+namespace Vivaply.API.Controllers
+{
+    [ApiController]
+    public abstract class BaseApiController : ControllerBase
+    {
+        protected Guid CurrentUserId
+        {
+            get
+            {
+                var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                if (!Guid.TryParse(userIdString, out var userId))
+                    throw new UnauthorizedAccessException();
+
+                return userId;
+            }
+        }
+    }
+}
